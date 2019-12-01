0ea2ce7543c5192f465fb268cb0f0dac16d92bec
mlfoste1 <mlfoste1@users.noreply.github.com>
2019-12-01 16:04:52 -0500

    Project 3 Updates

--- Project_3/master_df.R
+++ Project_3/master_df.R
@@ -4,111 +4,109 @@ library(stringr)
 
 
 
-#----------------------------------------------------------------------------------------------
-
-#Create dataset-----------------------------------------
-#load data file in Durham Count Point in Time Data
-master_df = read_csv("https://raw.githubusercontent.com/datasci611/bios611-projects-fall-2019-mlfoste1/master/Project_3/data/master.csv", na = '**')
-
-master_df$n <- as.numeric(master_df$count_)
-view(master_df)
+#load master data file
+master_df = read_csv("https://raw.githubusercontent.com/datasci611/bios611-projects-fall-2019-mlfoste1/master/Project_3/data/master.csv", na = 'NaN')
 
 
-#Visuals------------------------------------------------
+#Create datasets to understand veteran demographics----------------------------------------------------------------------------------------------
 
-#Race by year
-master_race_yr = ggplot(tally(group_by(Race)),
-  aes(x = Race, y = n, fill = Race)) +
-  geom_bar(stat="identity") + labs(fill="Race")
+#---veteran status by gender
 
-master_race_yr
+veteranstatus_gender_df = master_df %>%
+  filter(VeteranStatus %in% c("Yes (HUD)","No (HUD)")) %>%
+  filter(Gender %in% c("Female","Male","Trans Female (MTF or Male to Female"))
+#veteranstatus_gender_df
 
-#Total Number Services by Family Size by Year
-ggplot(final_PIT_df, aes(x=Year_of_service, y=n, group=Indv_Family)) +
-  geom_line(aes(color=Indv_Family)) +
-  scale_x_discrete() +
-  xlab("Year_of_Service") + 
-  ggtitle('Durham PIT - Total Number Services by Family Size by Year')
+veteranstatus_gender_plot = ggplot(veteranstatus_gender_df, aes(x = Gender, fill = VeteranStatus)) +
+  geom_bar()
+#veteranstatus_gender_plot
 
+#---veteran status by Race
+veteranstatus_race_df = master_df %>%
+  filter(VeteranStatus %in% c("Yes (HUD)","No (HUD)")) %>%
+  filter(Race %in% c("White (HUD)",
+"Black or African American (HUD)",
+"American Indian or Alaska Native (HUD)",
+"Asian (HUD)",
+"Native Hawaiian or Other Pacific Islander (HUD)",
+"Client doesnt know (HUD)",
+"Data not collected (HUD)",
+"Client refused (HUD)") 
+)
+#veteranstatus_race_df
 
-#visuals
-
-#select distinct clientfilenum and  max family size determined by food provided for
-
-group_UMD_df = final_UMD_df %>%
-  group_by(Client_File_Number) %>%
-  filter(Food_Provided_for > 0, Food_Provided_for <= 10)  %>%
-  summarise(max_Food_Provided_for = max(Food_Provided_for)) %>%
-  drop_na(max_Food_Provided_for)
-
-View(group_UMD_df)
-
-#Create groups
-group_UMD_df$Family_Size <- cut(group_UMD_df$max_Food_Provided_for, breaks = c(0,1,2,3,4,5,6,7,10), labels=c("Individual","2","3","4","5","6","7","8+"))
-
-group_UMD_df$Indv_Family <- cut(group_UMD_df$max_Food_Provided_for, breaks = c(0,1,10), labels=c("Individual","Family"))
+veteranstatus_race_plot = ggplot(veteranstatus_race_df, aes(x = Race, fill = VeteranStatus)) +
+  geom_bar() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+#veteranstatus_race_plot
 
 
-#view(group_UMD_df)
+#---veteran status by Ethnicity
+veteranstatus_ethnicity_df = master_df %>%
+  filter(VeteranStatus %in% c("Yes (HUD)","No (HUD)")) %>%
+  filter(Ethnicity %in% c("Non-Hispanic/Non-Latino (HUD)",
+"Hispanic/Latino (HUD)"
+))
+#veteranstatus_ethnicity_df
 
-#Counts by Family Size (2007-2016)
-ggplot(group_UMD_df, aes(x=Family_Size)) +
-  geom_bar(aes(fill=Family_Size)) +
-  xlab("Family Size") + 
-  ggtitle('Counts by Family Size (2007-2016)')
-
-#Individuals Versus Families (2007-2016)
-ggplot(group_UMD_df, aes(x=Indv_Family), group = Indv_Family) +
-  geom_bar(aes(fill=Indv_Family)) +
-  xlab("Family Size") + 
-  ggtitle('Individuals Versus Families (2007-2016)')
-
-#view(group_UMD_df)
-
-#join to original data set to append Date_of_Service
-
-#view by each family size
-family_size_group_UMD_df = inner_join(group_UMD_df, final_UMD_df, by = c("Client_File_Number" = "Client_File_Number"), suffix = c(".x", ".y")) %>%
-  mutate(Year_of_Service = format(Date_of_service, "%Y")) %>%
-  group_by(Family_Size, Year_of_Service) %>%
-  summarise(n=n_distinct(Client_File_Number))
+veteranstatus_ethnicity_plot = ggplot(veteranstatus_ethnicity_df, aes(x = Ethnicity, fill = VeteranStatus)) +
+  geom_bar()
+#veteranstatus_ethnicity_plot
 
 
-view(family_size_group_UMD_df)
+#---veteran status by Age
+veteranstatus_age_df = master_df %>%
+  filter(VeteranStatus %in% c("Yes (HUD)","No (HUD)")) %>%
+ drop_na(Age)
+#veteranstatus_age_df
 
-#Total Number Services by Family Size by Year
-ggplot(family_size_group_UMD_df, aes(x=Year_of_Service, y=n, group=Family_Size)) +
-  geom_line(aes(color=Family_Size)) +
-  scale_x_discrete() +
-  xlab("Year_of_Service") + 
-  ylab("Number Serviced") + 
-  ggtitle('Total Number Serviced by Family Size by Year')
+veteranstatus_age_plot = ggplot(veteranstatus_age_df, aes(x = VeteranStatus, y = Age)) +
+  geom_boxplot()
+#veteranstatus_age_plot
+
+max_Age = master_df %>%
+  drop_na(Age) %>%
+  summarise(find_max_age = max(Age))
+#max_Age
+
+min_Age = master_df %>%
+  drop_na(Age) %>%
+  summarise(find_min_age = min(Age))
+#min_Age
 
 
-#view by individual vs family
-Indv_Family_group_UMD_df = inner_join(group_UMD_df, final_UMD_df, by = c("Client_File_Number" = "Client_File_Number"), suffix = c(".x", ".y")) %>%
-  mutate(Year_of_Service = format(Date_of_service, "%Y")) %>%
-  group_by(Indv_Family, Year_of_Service) %>%
-  summarise(n=n_distinct(Client_File_Number))
 
-view(Indv_Family_group_UMD_df)
 
-#Individual vs Family by Year
-ggplot(Indv_Family_group_UMD_df, aes(x=Year_of_Service, y=n, group=Indv_Family)) +
-  geom_line(aes(color=Indv_Family)) +
-  scale_x_discrete() +
-  xlab("Year_of_Service") +
-  ylab("Number Serviced") +
-  ggtitle('Individual vs Family by Year')
 
-#---------------------------------------------------------------------------------
-#Overlap data
 
-#Number serviced vs number reported
-  ggplot(Indv_Family_group_UMD_df, aes(x=Year_of_Service, y=n, group=Indv_Family)) +
-    geom_line(size=1.5, aes(color=Indv_Family)) +
-    geom_line(data=final_PIT_df, aes(x=Year_of_service, y=n, group=Indv_Family, color=measures)) +
-    xlab("Year of Service") +
-    ylab("Number Serviced") +
-    ggtitle('Number of People Serviced vs Number Reported in Durham PIT Count')
+#Create datasets to understand disabilities----------------------------------------------------------------------------------------------
+
+#load master data file
+disability_df = read_csv("https://raw.githubusercontent.com/datasci611/bios611-projects-fall-2019-mlfoste1/master/Project_3/data/disability.csv", na = 'NaN')
+#disability_df
+
+disability_plot = ggplot(disability_df, aes(x = DisabilityType, fill = DisabilityType)) +
+  geom_bar() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+  theme_minimal()
+
+#disability_plot
+
+disability_master_df = inner_join(disability_df, master_df, by = c("ClientID" = "ClientID"), suffix = c(".x", ".y")) %>%
+  filter(VeteranStatus %in% c("Yes (HUD)","No (HUD)"))
+#disability_master_df
+
+disability_master_plot = ggplot(disability_master_df, aes(x = DisabilityType, fill=VeteranStatus)) +
+  geom_bar() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+#disability_master_plot
+
+disability_veteran_df = inner_join(disability_df, master_df, by = c("ClientID" = "ClientID"), suffix = c(".x", ".y")) %>%
+  filter(VeteranStatus %in% c("Yes (HUD)"))
+#disability_veteran_df
+
+disability_veteran_plot = ggplot(disability_veteran_df, aes(x = DisabilityType, y=Age)) +
+  geom_boxplot() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+#disability_veteran_plot
 
